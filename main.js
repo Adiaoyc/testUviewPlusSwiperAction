4ac8858b909fec8e39a95fbf2ca4eb034dfe1fb3
diaoyc <diaoyongcheng392@163.com>
2024-04-13 22:28:18 +0800

    init

---
+++ main.js
@@ -0,0 +1,30 @@
+import App from './App'
+
+import uviewPlus from '@/uni_modules/uview-plus'
+
+// #ifndef VUE3
+import Vue from 'vue'
+import './uni.promisify.adaptor'
+Vue.config.productionTip = false
+App.mpType = 'app'
+const app = new Vue({
+  ...App
+})
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import { createSSRApp } from 'vue'
+export function createApp() {
+  const app = createSSRApp(App)
+  
+  // 使用uviewPlus
+  app.use(uviewPlus)
+  // 配置uviewPlus的默认单位为rpx
+  uni.$u.config.unit = 'rpx'
+  
+  return {
+    app
+  }
+}
+// #endif
---
+++ pages/index/index.vue
@@ -0,0 +1,38 @@
+<template>
+	<view>
+
+
+		<up-swipe-action :autoClose="true">
+			<up-swipe-action-item :options="options1">
+				<view style="height: 100px; background-color: aqua;">
+					111111111
+				</view>
+			</up-swipe-action-item>
+			<up-swipe-action-item :options="options1">
+				<view style="height: 100px; background-color: aqua;">
+					111111111
+				</view>
+			</up-swipe-action-item>
+
+		</up-swipe-action>
+
+	</view>
+</template>
+
+<script setup>
+	import {
+		ref,
+		reactive
+	} from "vue";
+	
+	let options1 = reactive([{
+		text: '删除',
+		style: {
+			backgroundColor: '#dd524d'
+		}
+	}]);
+</script>
+
+<style lang="scss" scoped>
+
+</style>
